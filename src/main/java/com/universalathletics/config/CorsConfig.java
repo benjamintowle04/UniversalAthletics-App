749e74cb7577d2fd5b9f6476e6ccbd9b3312427e
benjamintowle04 <benjamintowle04@gmail.com>
2026-01-26 13:09:47 -0600

    Cors: use jakarta.servlet imports for logging filter

--- src/main/java/com/universalathletics/config/CorsConfig.java
+++ src/main/java/com/universalathletics/config/CorsConfig.java
@@ -10,12 +10,12 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
-import javax.servlet.Filter;
-import javax.servlet.ServletException;
-import javax.servlet.ServletRequest;
-import javax.servlet.ServletResponse;
-import javax.servlet.FilterChain;
-import javax.servlet.http.HttpServletRequest;
+import jakarta.servlet.Filter;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.http.HttpServletRequest;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
